d83b8a315503cb1412d50361f3dda5b59d732bed
Aya <ayabeltran14@gmail.com>
2019-01-13 21:18:41 +0800

    added recyclerview on mapviewpage

---
+++ app/src/main/java/com/example/ayabeltran/conitracking/ChildListDisplay.java
@@ -0,0 +1,13 @@
+package com.example.ayabeltran.conitracking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChildListDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_list_display);
+    }
+}
